6038cb4ef55cb03f45399fcdce76b9df86364de9
zaidan7 <160464485+zaidan7@users.noreply.github.com>
2024-10-07 13:05:05 +0500

    show

---
+++ admin/showauthor.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        
+        </style>
+</head>
+<body>
+    <?php include "header.php"?>
+    <br><br>
+    <div class="container">
+      <h1>AUTHOR</h1>
+      <table class="table table-bordered">
+        <tr>
+            <th>ID</th>
+            <th>NAME</th>
+            <th>EMAIL</th>
+            <th>GENDER</th>
+            <th>AGE</th>
+            <th>UPDATE</th>
+            <th>DELELTE</th>
+        </tr>
+        <?php
+        $query="SELECT * FROM author_info";
+        $result=mysqli_query($conn,$query);
+        while($row=mysqli_fetch_array($result))
+        {
+          echo"<tr>";
+          echo"<td>".$row[0]."</td>"; 
+          echo"<td>".$row[1]."</td>"; 
+          echo"<td>".$row[2]."</td>"; 
+          echo"<td>".$row[3]."</td>"; 
+          echo"<td>".$row[4]."</td>";  
+          echo'<td><a href="updatecategory.php?update='.$row[0].'"><i class="bi bi-pencil-square"</a></td>';
+          echo'<td><a href="deletecategory.php?delete='.$row[0].'"><i class="bi bi-trash3-fill"</a></td>';
+        }
+
+        ?>
+        
+
+      </table>  
+    </div> 
+   <?php include "footer.php"?>
+</body>
+</html>

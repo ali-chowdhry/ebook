b9da5fd28532e0db695b4dda3ff27c178d1631aa
zaidan7 <160464485+zaidan7@users.noreply.github.com>
2024-10-22 20:07:12 +0500

    Update Register.php

--- login/Register.php
+++ login/Register.php
@@ -1,126 +1,58 @@
 <?php include("../connection.php"); ?>
 <!DOCTYPE html>
-<html lang="en">
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>RegistrationForm_v10 by Colorlib</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <script type="text/javascript"
-        src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/8.0.0/mdb.umd.min.js"></script>
-    <link rel="stylesheet" href="style.css">
-</head>
+		<!-- LINEARICONS -->
+		<link rel="stylesheet" href="fonts/linearicons/style.css">
+		
+		<!-- STYLE CSS -->
+		<link rel="stylesheet" href="css/style.css">
+	</head>
 
-<body>
-    <section class="h-100 gradient-form" style="background-color: #eee;">
-        <div class="container py-5 h-100">
-            <div class="row d-flex justify-content-center align-items-center h-100">
-                <div class="col-xl-10">
-                    <div class="card rounded-3 text-black">
-                        <div class="row g-0">
-                            <div class="col-lg-6">
-                                <div class="card-body p-md-5 mx-md-4">
+	<body>
 
-                                    <div class="text-center">
-                                        <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/lotus.webp"
-                                            style="width: 185px;" alt="logo">
-                                        <h4 class="mt-1 mb-5 pb-1">We are The Lotus Team</h4>
-                                    </div>
-
-                                    <form action="" method="post">
-                                        <p>Please login to your account</p>
-
-                                        <div data-mdb-input-init class="form-outline mb-4">
-                                            <label class="form-label" for="form2Example11">Name</label>
-                                            <input type="text" id="form2Example11" name="" class="form-control"
-                                                placeholder="email address" />
-
-                                        </div>
-
-
-                                        <div data-mdb-input-init class="form-outline mb-4">
-                                            <label class="form-label" for="form2Example11">Email</label>
-                                            <input type="email" id="form2Example11" name="email" class="form-control"
-                                                placeholder="email address" />
-
-                                        </div>
-
-                                        <div data-mdb-input-init class="form-outline mb-4">
-                                            <label class="form-label" for="form2Example22">Password</label>
-                                            <input type="password" name="password" id="form2Example22"
-                                                class="form-control" />
-
-                                        </div>
-
-                                        <div data-mdb-input-init class="form-outline mb-4">
-                                            <label class="form-label" for="form2Example22">Gender</label>
-                                            <br>
-                                            <input type="radio" name="gender" value="male" 
-                                                 />male
-
-                                                <input type="radio" name="gender" value="female"
-                                                 />female
-
-                                        </div>
-
-                                        <div data-mdb-input-init class="form-outline mb-4">
-                                            <label class="form-label" for="form2Example22">Address</label>
-                                            <input type="text" name="password" id="form2Example22"
-                                                class="form-control" />
-
-                                        </div>
-
-                                        <div data-mdb-input-init class="form-outline mb-4">
-                                            <label class="form-label" for="form2Example22">Phone</label>
-                                            <input type="text" name="password" id="form2Example22"
-                                                class="form-control" />
-
-                                        </div>
-
-                                        <div data-mdb-input-init class="form-outline mb-4">
-                                            <label class="form-label" for="form2Example22">Role</label>
-                                            <input type="text" name="password" id="form2Example22"
-                                                class="form-control" />
-
-                                        </div>
-
-                                        <div class="text-center pt-1 mb-5 pb-1">
-                                            <button data-mdb-button-init data-mdb-ripple-init name="loginbtn"
-                                                class="btn btn-primary btn-block fa-lg gradient-custom-2 mb-3"
-                                                type="submit">Log
-                                                in</button>
-
-                                        </div>
-
-                                        <div class="d-flex align-items-center justify-content-center pb-4">
-                                            <p class="mb-0 me-2">Don't have an account?</p>
-                                            <button type="button" data-mdb-button-init data-mdb-ripple-init
-                                                class="btn btn-outline-danger">Create new</button>
-                                        </div>
-
-                                    </form>
-
-                                </div>
-                            </div>
-                            <div class="col-lg-6 d-flex align-items-center gradient-custom-2">
-                                <div class="text-white px-3 py-4 p-md-5 mx-md-4">
-                                    <h4 class="mb-4">We are more than just a company</h4>
-                                    <p class="small mb-0">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed
-                                        do eiusmod
-                                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                                        quis nostrud
-                                        exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
-</body>
+		<div class="wrapper">
+			<div class="inner">
+				<img src="images/image-1.png" alt="" class="image-1">
+				<form action="">
+					<h3>New Account?</h3>
+					<div class="form-holder">
+						<span class="lnr lnr-user"></span>
+						<input type="text" class="form-control" placeholder="Username">
+					</div>
+					<div class="form-holder">
+						<span class="lnr lnr-phone-handset"></span>
+						<input type="text" class="form-control" placeholder="Phone Number">
+					</div>
+					<div class="form-holder">
+						<span class="lnr lnr-envelope"></span>
+						<input type="text" class="form-control" placeholder="Mail">
+					</div>
+					<div class="form-holder">
+						<span class="lnr lnr-lock"></span>
+						<input type="password" class="form-control" placeholder="Password">
+					</div>
+					<div class="form-holder">
+						<span class="lnr lnr-lock"></span>
+						<input type="password" class="form-control" placeholder="Confirm Password">
+					</div>
+					<button>
+						<span>Register</span>
+					</button>
+				</form>
+				<img src="images/image-2.png" alt="" class="image-2">
+			</div>
+			
+		</div>
+		
+		<script src="js/jquery-3.3.1.min.js"></script>
+		<script src="js/main.js"></script>
+	</body><!-- This templates was made by Colorlib (https://colorlib.com) -->
+</html>
 <?php
 session_start();
 if (isset($_POST["loginbtn"])) {
@@ -148,4 +80,3 @@ if (isset($_POST["loginbtn"])) {
 ?>
 
 
-</html>
